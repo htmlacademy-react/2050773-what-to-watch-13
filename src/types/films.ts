fc8eac5e8de478b1c7019472a133a97ed51beca2
Anastasiia Chasovskikh <nastya.chasovskikh@gmail.com>
2023-09-15 13:35:05 -0700

    добавляет моковые данные

---
+++ src/types/films.ts
@@ -0,0 +1,17 @@
+export type Films = {
+  id: string;
+  name: string;
+  posterImage: string;
+  backgroundImage: string;
+  backgroundColor: string;
+  videoLink: string;
+  description: string;
+  rating: number;
+  scoresCount: number;
+  director: string;
+  starring: string[];
+  runTime: number;
+  genre: string;
+  released: number;
+  isFavorite: boolean;
+};
