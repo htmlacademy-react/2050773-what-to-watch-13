92a8d2b4818d2400b4d6862351124faf5cb6c599
Anastasiia Chasovskikh <nastya.chasovskikh@gmail.com>
2023-09-15 20:07:03 -0700

    добавляет вкладки overview, detail, reviews WIP

---
+++ src/components/tabs/tabs.tsx
@@ -0,0 +1,36 @@
+import { TTab } from '../../types/tabs';
+
+type TabsProps = {
+  activeTab: TTab;
+  onTabClick: (tab: TTab) => void;
+}
+
+
+function Tabs({activeTab, onTabClick}: TabsProps): JSX.Element {
+  return(
+    <nav className="film-nav film-card__nav">
+      <ul className="film-nav__list">
+        {['Overview', 'Details', 'Reviews'].map((tab) => (
+          <li
+            className={`film-nav__item ${activeTab === tab ? 'film-nav__item--active' : ''}`}
+            key={tab}
+          >
+            <a
+              href="#"
+              className="film-nav__link"
+              onClick={(e) => {
+                e.preventDefault();
+                onTabClick(tab as TTab);
+              }}
+            >
+              {tab}
+            </a>
+          </li>
+        ))}
+
+      </ul>
+    </nav>
+  );
+}
+
+export default Tabs;
--- src/pages/film-screen/film-screen.tsx
+++ src/pages/film-screen/film-screen.tsx
@@ -2,15 +2,174 @@ import Header from '../../components/header/header';
 import { Helmet } from 'react-helmet-async';
 import { TFilms } from '../../types/films';
 import Footer from '../../components/footer/footer';
+import Tabs from '../../components/tabs/tabs';
+import { TTab } from '../../types/tabs';
+import { useState } from 'react';
 
 type FilmCardProps = {
   film: TFilms;
 };
 
 function FilmScreen({film}: FilmCardProps): JSX.Element {
-  const { name, description, director,
-    rating, scoresCount, starring, genre, released } = film;
+  const { name, description, director, rating, scoresCount, starring, genre, released } = film;
   const formattedStarring = starring.join(', ');
+
+  const [activeTab, setActiveTab] = useState<TTab>('Overview');
+
+  const handleTabClick = (tab: TTab) => {
+    setActiveTab(tab);
+  };
+
+  const renderActiveTabFilmInfo = () => {
+    switch(activeTab) {
+      case 'Overview':
+        return (
+          <>
+            <div className="film-rating">
+              <div className="film-rating__score">{rating}</div>
+              <p className="film-rating__meta">
+                <span className="film-rating__level">Very good</span>
+                <span className="film-rating__count">{scoresCount} ratings</span>
+              </p>
+            </div>
+            <div className="film-card__text">
+              <p>{description}</p>
+              <p className="film-card__director"><strong>Director: {director}</strong></p>
+              <p className="film-card__starring"><strong>Starring: {formattedStarring}, and other</strong></p>
+            </div>
+          </>
+        );
+      case 'Details':
+        return (
+          <div className="film-card__text film-card__row">
+            <div className="film-card__text-col">
+              <p className="film-card__details-item">
+                <strong className="film-card__details-name">Director</strong>
+                <span className="film-card__details-value">Wes Anderson</span>
+              </p>
+              <p className="film-card__details-item">
+                <strong className="film-card__details-name">Starring</strong>
+                <span className="film-card__details-value">
+                    Bill Murray, <br/>
+                    Edward Norton, <br/>
+                    Jude Law, <br/>
+                    Willem Dafoe, <br/>
+                    Saoirse Ronan, <br/>
+                    Tony Revoloru, <br/>
+                    Tilda Swinton, <br/>
+                    Tom Wilkinson, <br/>
+                    Owen Wilkinson, <br/>
+                    Adrien Brody, <br/>
+                    Ralph Fiennes, <br/>
+                    Jeff Goldblum
+                </span>
+              </p>
+            </div>
+            <div className="film-card__text-col">
+              <p className="film-card__details-item">
+                <strong className="film-card__details-name">Run Time</strong>
+                <span className="film-card__details-value">1h 39m</span>
+              </p>
+              <p className="film-card__details-item">
+                <strong className="film-card__details-name">Genre</strong>
+                <span className="film-card__details-value">Comedy</span>
+              </p>
+              <p className="film-card__details-item">
+                <strong className="film-card__details-name">Released</strong>
+                <span className="film-card__details-value">2014</span>
+              </p>
+            </div>
+          </div>
+        );
+      case 'Reviews':
+        return (
+          <div className="film-card__reviews film-card__row">
+            <div className="film-card__reviews-col">
+              <div className="review">
+                <blockquote className="review__quote">
+                  <p className="review__text">Discerning travellers and Wes Anderson fans will luxuriate in the glorious Mittel-European kitsch of one of the directors funniest and most exquisitely designed films in years.</p>
+
+                  <footer className="review__details">
+                    <cite className="review__author">Kate Muir</cite>
+                    <time className="review__date" dateTime="2016-12-24">December 24, 2016</time>
+                  </footer>
+                </blockquote>
+
+                <div className="review__rating">8,9</div>
+              </div>
+
+              <div className="review">
+                <blockquote className="review__quote">
+                  <p className="review__text">Andersons films are too precious for some, but for those of us willing to lose ourselves in them, they are a delight. The Grand Budapest Hotel is no different, except that he has added a hint of gravitas to the mix, improving the recipe.</p>
+
+                  <footer className="review__details">
+                    <cite className="review__author">Bill Goodykoontz</cite>
+                    <time className="review__date" dateTime="2015-11-18">November 18, 2015</time>
+                  </footer>
+                </blockquote>
+
+                <div className="review__rating">8,0</div>
+              </div>
+
+              <div className="review">
+                <blockquote className="review__quote">
+                  <p className="review__text">I did not find it amusing, and while I can appreciate the creativity, it is an hour and 40 minutes I wish I could take back.</p>
+
+                  <footer className="review__details">
+                    <cite className="review__author">Amanda Greever</cite>
+                    <time className="review__date" dateTime="2015-11-18">November 18, 2015</time>
+                  </footer>
+                </blockquote>
+
+                <div className="review__rating">8,0</div>
+              </div>
+            </div>
+            <div className="film-card__reviews-col">
+              <div className="review">
+                <blockquote className="review__quote">
+                  <p className="review__text">The mannered, madcap proceedings are often delightful, occasionally silly, and here and there, gruesome and/or heartbreaking.</p>
+
+                  <footer className="review__details">
+                    <cite className="review__author">Matthew Lickona</cite>
+                    <time className="review__date" dateTime="2016-12-20">December 20, 2016</time>
+                  </footer>
+                </blockquote>
+
+                <div className="review__rating">7,2</div>
+              </div>
+
+              <div className="review">
+                <blockquote className="review__quote">
+                  <p className="review__text">It is certainly a magical and childlike way of storytelling, even if the content is a little more adult.</p>
+
+                  <footer className="review__details">
+                    <cite className="review__author">Paula Fleri-Soler</cite>
+                    <time className="review__date" dateTime="2016-12-20">December 20, 2016</time>
+                  </footer>
+                </blockquote>
+
+                <div className="review__rating">7,6</div>
+              </div>
+
+              <div className="review">
+                <blockquote className="review__quote">
+                  <p className="review__text">It is certainly a magical and childlike way of storytelling, even if the content is a little more adult.</p>
+
+                  <footer className="review__details">
+                    <cite className="review__author">Paula Fleri-Soler</cite>
+                    <time className="review__date" dateTime="2016-12-20">December 20, 2016</time>
+                  </footer>
+                </blockquote>
+
+                <div className="review__rating">7,0</div>
+              </div>
+            </div>
+          </div>
+        );
+      default:
+        return null;
+    }
+  };
 
 
   return(
@@ -59,36 +218,9 @@ function FilmScreen({film}: FilmCardProps): JSX.Element {
             </div>
 
             <div className="film-card__desc">
-              <nav className="film-nav film-card__nav">
-                <ul className="film-nav__list">
-                  <li className="film-nav__item film-nav__item--active">
-                    <a href="#" className="film-nav__link">Overview</a>
-                  </li>
-                  <li className="film-nav__item">
-                    <a href="#" className="film-nav__link">Details</a>
-                  </li>
-                  <li className="film-nav__item">
-                    <a href="#" className="film-nav__link">Reviews</a>
-                  </li>
-                </ul>
-              </nav>
+              <Tabs activeTab={activeTab} onTabClick={handleTabClick} />
+              {renderActiveTabFilmInfo()}
 
-              <div className="film-rating">
-                <div className="film-rating__score">{rating}</div>
-                <p className="film-rating__meta">
-                  <span className="film-rating__level">Very good</span>
-                  <span className="film-rating__count">{scoresCount} ratings</span>
-                </p>
-              </div>
-
-              <div className="film-card__text">
-                <p>{description}</p>
-
-
-                <p className="film-card__director"><strong>Director: {director}</strong></p>
-
-                <p className="film-card__starring"><strong>Starring: {formattedStarring}, and other</strong></p>
-              </div>
             </div>
           </div>
         </div>
