af10df62a58af82ecfd6650fe134f74532241734
Anastasiia Chasovskikh <nastya.chasovskikh@gmail.com>
2023-09-21 14:16:17 -0700

    исправляет ссылку футера на главной странице

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,12 +1,30 @@
+import { Link } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
+import { AppRoute } from '../../const';
+
 function Footer(): JSX.Element {
+  const location = useLocation();
+  const isIndexPage = location.pathname === AppRoute.Root;
+
   return(
     <footer className="page-footer">
       <div className="logo">
-        <a href="main.html" className="logo__link logo__link--light">
-          <span className="logo__letter logo__letter--1">W</span>
-          <span className="logo__letter logo__letter--2">T</span>
-          <span className="logo__letter logo__letter--3">W</span>
-        </a>
+        {
+          isIndexPage ? (
+            <a className="logo__link logo__link--light">
+              <span className="logo__letter logo__letter--1">W</span>
+              <span className="logo__letter logo__letter--2">T</span>
+              <span className="logo__letter logo__letter--3">W</span>
+            </a>
+          ) : (
+            <Link to={AppRoute.Root} className="logo__link logo__link--light">
+              <span className="logo__letter logo__letter--1">W</span>
+              <span className="logo__letter logo__letter--2">T</span>
+              <span className="logo__letter logo__letter--3">W</span>
+            </Link>
+          )
+        }
+
       </div>
 
       <div className="copyright">
